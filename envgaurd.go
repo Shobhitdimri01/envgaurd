6273dd7e132d030c1400126a5a7e75dc381d35b4
Shobhit Dimri <shobhitdimri@Shobhits-MacBook-Pro.local>
2025-04-23 10:40:08 +0530

    added most of the functions

--- envgaurd.go
+++ envgaurd.go
@@ -10,6 +10,7 @@ import (
 )
 
 // Load manually loads the .env file into environment variables
+// Only sets environment variable if not already set it doesn't override
 func Load(filepath string) error {
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -29,7 +30,12 @@ func Load(filepath string) error {
 		if len(parts) != 2 {
 			continue //invalid format, skip
 		}
-		os.Setenv(parts[0], parts[1])
+		key := strings.TrimSpace(parts[0])
+		val := strings.Trim(strings.TrimSpace(parts[1]), `"'`)
+		if os.Getenv(key) == "" { // Only set if not already set
+			os.Setenv(key, val)
+		}
+
 	}
 	if err := scanLine.Err(); err != nil {
 		return fmt.Errorf("error reading .env file: %v", err)
@@ -45,11 +51,198 @@ func Required(key string) {
 	}
 }
 
+// OverLoad overwrite all the existing environment variable
+func OverLoad(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("unable to open .env file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.Trim(strings.TrimSpace(parts[1]), `"'`)
+		os.Setenv(key, val)
+	}
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error reading .env file: %v", err)
+	}
+	return nil
+}
+
+// GetInt extract Value from env dot file and return Integer Value if not given returns default value
 func GetInt(key string, def int) int {
 	value := os.Getenv(key)
 	if value == "" {
 		return def
 	}
 	val := validate.Validate(value, def)
-	return val.(int)
+	IntVal, ok := val.(int)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return IntVal
 }
+
+// GetStr extracts a value from the environment variables and returns it as a string.
+// If the key is not found or the value is empty, it returns the provided default.
+func GetStr(key string, def string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(value, def)
+	strVal, ok := val.(string)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return strVal
+}
+
+func GetBool(key string, def bool) bool {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(value, def)
+	BoolVal, ok := val.(bool)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return BoolVal
+}
+
+// GetFloat retrieves a float value from the environment, with a default if not found
+func GetFloat64(key string, def float64) float64 {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(value, def)
+	FloatVal, ok := val.(float64)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return FloatVal
+}
+
+// GetStringArray retrieves a comma-separated list of strings from the environment
+// and returns them as a slice of strings
+func GetStringArray(key string, def []string) []string {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(value, def)
+	strArrayVal, ok := val.([]string)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return strArrayVal
+}
+
+// GetIntegerArray retrieves a comma-separated list of strings from the environment
+// and returns them as a slice of integer
+func GetIntegerArray(key string, def []int) []int {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(value, def)
+	intArrayVal, ok := val.([]int)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return intArrayVal
+}
+
+// GetEnvAsMap retrieves a JSON-encoded map from an env var, or returns default if not found or invalid
+func GetEnvAsMap(key string, def map[string]any) map[string]any {
+	value := os.Getenv(key)
+	if value == "" {
+		return def
+	}
+	val := validate.Validate(key, def)
+	mapValues, ok := val.(map[string]any)
+	if !ok {
+		return def // fallback if type assertion fails
+	}
+	return mapValues
+}
+
+// LoadFromFileWithValidation loads environment variables from a file and validates them
+func LoadFromFileWithValidation(filepath string, requiredKeys []string) error {
+	if err := Load(filepath); err != nil {
+		return err
+	}
+	for _, key := range requiredKeys {
+		Required(key) // Check if required keys are present
+	}
+	return nil
+}
+
+// PrintEnvVars is a utility function that iterates through all the environment variables of the current process and prints them in a formatted manner. For any environment variable deemed sensitive (e.g., containing secrets, tokens, passwords),
+// its value is masked before printing to prevent leaking sensitive information.
+func PrintEnvVars() {
+	for _, env := range os.Environ() {
+		parts := strings.SplitN(env, "=", 2)
+		key := parts[0]
+		val := parts[1]
+		if isSensitive(key) {
+			val = maskValue(val)
+		}
+		fmt.Printf("%-20s = %s\n", key, val)
+	}
+}
+
+// Checks if the key should be masked in log output
+func isSensitive(key string) bool {
+	key = strings.ToUpper(key)
+	for k := range MaskingKeys {
+		if strings.Contains(key, k) {
+			return true
+		}
+	}
+	return false
+}
+
+var MaskingKeys = make(map[string]bool)
+
+// Masking lets users define which keys are considered sensitive will be shown encrypted(eg.xxxx)
+func Masking(keys ...string) {
+	for _, key := range keys {
+		MaskingKeys[strings.ToUpper(key)] = true
+	}
+}
+
+// Masks the value for printing
+func maskValue(val string) string {
+	if len(val) <= 4 {
+		return strings.Repeat("*", len(val))
+	}
+	return val[:2] + strings.Repeat("*", len(val)-4) + val[len(val)-2:]
+}
+
+/*
+Problem | Default Go | envgaurd Vision
+Type safety | ❌ | ✅ GetInt, GetBool
+Validation | ❌ | ✅ Required/Optional checks
+Defaults | ❌ | ✅ Inline defaults
+.env loading | ❌ | ✅ Built-in
+Security masking | ❌ | ✅ Sensitive var protection
+Multiple environments | ❌ | ✅ Profiles/namespaces
+Centralized error handling | ❌ | ✅ All at once validation
+
+*/
--- internal/validate/validate.go
+++ internal/validate/validate.go
@@ -1,12 +1,14 @@
 package validate
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
-func Validate(value string, def interface{}) interface{} {
+func Validate(value string, def any) any {
 	switch def.(type) {
 	case int:
 		val, err := strconv.Atoi(value)
@@ -22,7 +24,75 @@ func Validate(value string, def interface{}) interface{} {
 		return val
 	case string:
 		return value
+	case float64:
+		val, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			panic("invalid value expected float64 type")
+		}
+		return val
+	case []string:
+		val, err := toStringSlice(value)
+		if err != nil {
+			panic(err.Error())
+		}
+		return val
+	case []int:
+		val, err := toIntegerSlice(value)
+		if err != nil {
+			panic(err.Error())
+		}
+		return val
+	case map[string]any:
+		val, err := toJsonMap(value)
+		if err != nil {
+			panic(err)
+		}
+		return val
 	default:
 		panic(fmt.Sprintf("Unsupported type: %v", reflect.TypeOf(def)))
 	}
+}
+
+func toStringSlice(val any) ([]string, error) {
+	s, ok := val.(string)
+	if !ok {
+		return nil, fmt.Errorf("invalid data type not string")
+	}
+	commaValues := strings.Split(s, ",")
+	if len(commaValues) < 1 {
+		return nil, fmt.Errorf("comma missing in defining value")
+	}
+	var strArrayVal []string
+	strArrayVal = append(strArrayVal, commaValues...)
+	return strArrayVal, nil
+}
+
+func toIntegerSlice(val any) ([]int, error) {
+	s, ok := val.(string)
+	if !ok {
+		return nil, fmt.Errorf("invalid data type not integer")
+	}
+	commaValues := strings.Split(s, ",")
+	if len(commaValues) < 1 {
+		return nil, fmt.Errorf("comma missing in defining value")
+	}
+	var numIntArr []int
+
+	for _, v := range commaValues {
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			continue
+		}
+		numIntArr = append(numIntArr, val)
+	}
+	return numIntArr, nil
+}
+
+func toJsonMap(value string) (map[string]any, error) {
+	var jsonVal map[string]any
+	err := json.Unmarshal([]byte(value), jsonVal)
+	if err != nil {
+		return nil, fmt.Errorf("unable to convert json into map:%v", err)
+	}
+	return jsonVal, nil
 }
