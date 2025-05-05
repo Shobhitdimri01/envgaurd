ef57f607e7726cc539f044890c88d0ed57bf7375
Shobhit Dimri <shobhitdimri@Shobhits-MacBook-Pro.local>
2025-05-05 23:47:28 +0530

    syntax error2

--- envgaurd_test.go
+++ envgaurd_test.go
@@ -146,8 +146,8 @@ func TestPrintEnvVars(t *testing.T) {
 	}
 
 	expected := []string{
-		fmt.Sprintf("%-2s = %s", "DB_PASSWORD", "su*******et"), // 11 asterisks for "supersecret"
-		fm.Sprintf("%-2s = %s", "APP_ENV", "production"),
+		fm.Sprintf("%-2s = %s", "DB_PASSWORD", "su*******et"), // 11 asterisks for "supersecret"
+		fmt.Sprintf("%-2s = %s", "APP_ENV", "production"),
 	}
 
 	original := os.Stdout
